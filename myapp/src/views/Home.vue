c4d5f67af421c123b75f7d71354860c4ec5d9d16
ruizhouli <1772872909@qq.com>
2020-04-22 15:17:28 +0800

    256

---
+++ myapp/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    你好，世界
+    <ol>
+      <tem></tem>
+    </ol>
+  </div>
+  
+</template>
+
+<script>
+
+var tem={template:"<li>niap</li>"}
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  components:{
+    tem
+  }
+}
+</script>
